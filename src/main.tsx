e471e270e25d10b738680fe06a77509f2ead0702
Emyeeeel <maryamielcatado@gmail.com>
2024-09-04 19:19:55 +0800

    saving photos functionality added

--- src/main.tsx
+++ src/main.tsx
@@ -9,3 +9,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader before the render call
+defineCustomElements(window);
